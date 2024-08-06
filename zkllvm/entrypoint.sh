cb2d1c4d24b1fa7f6599b24e3cc15cf8f2eb0cf1
Mateusz Zając <matzayonc@gmail.com>
2024-08-06 12:25:43 +0200

    generating zkllvm proof

--- zkllvm/entrypoint.sh
+++ zkllvm/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Starting zkllvm"
+cargo +zkllvm build --release --target assigner-unknown-unknown --features=zkllvm
+
+assigner -b target/assigner-unknown-unknown/release/zkllvm-rust-template.ll -i inputs/example.inp -t assignment.tbl -c circuit.crct -e pallas
+
+proof-generator-multi-threaded --circuit circuit.crct --assignment assignment.tbl --proof proof.bin
+
+cat proof.bin
