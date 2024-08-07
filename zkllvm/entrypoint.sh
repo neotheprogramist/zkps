ff108839de47d93e7b62e5bffabb41e19ef39afd
Mateusz Zając <matzayonc@gmail.com>
2024-08-07 12:54:51 +0200

    zkllvm fibonacci

--- zkllvm/entrypoint.sh
+++ zkllvm/entrypoint.sh
@@ -2,16 +2,16 @@
 
 LOG_FILE=proving.log
 
-# Compile and generate vrifier
-cargo +zkllvm build --release --target assigner-unknown-unknown --features=zkllvm > $LOG_FILE 2> $LOG_FILE
-transpiler -m gen-evm-verifier -c circuit.crct -t assignment.tbl -o verifier -e pallas > $LOG_FILE
+# Compile and generate verifier
+cargo +zkllvm build --release --target assigner-unknown-unknown --features=zkllvm > $LOG_FILE 2> $LOG_FILE && \
+transpiler -m gen-evm-verifier -c circuit.crct -t assignment.tbl -o verifier -e pallas >> $LOG_FILE
 
 # Assign inputs
-assigner -b target/assigner-unknown-unknown/release/zkllvm-rust-template.ll -i inputs/example.inp -t assignment.tbl -c circuit.crct -e pallas > $LOG_FILE
+assigner -b target/assigner-unknown-unknown/release/zkllvm-rust-template.ll -i inputs/example.inp -t assignment.tbl -c circuit.crct -e pallas >> $LOG_FILE
 
 # Generate proof
-proof-generator-multi-threaded --circuit circuit.crct --assignment assignment.tbl --proof proof.bin > $LOG_FILE
+proof-generator-multi-threaded --circuit circuit.crct --assignment assignment.tbl --proof proof.bin >> $LOG_FILE
 
-tar -czf proof.tgz verifier circuit.crct proof.bin $LOG_FILE
+tar -czf proof.tgz verifier circuit.crct proof.bin $LOG_FILE 
 
 cat proof.tgz
--- zkllvm/src/main.rs
+++ zkllvm/src/main.rs
@@ -2,23 +2,20 @@
 #![feature(const_trait_impl)]
 #![feature(effects)]
 
-use ark_ff::{Field, MontFp};
+use ark_ff::MontFp;
 use ark_pallas::Fq;
-use unroll::unroll_for_loops;
-
-#[unroll_for_loops]
-fn pow_2(a: Fq) -> Fq {
-    let mut acc = Fq::ONE;
-    for i in 0..2 {
-        acc = acc * a;
-    }
-
-    acc
-}
 
 #[circuit]
-pub fn field_arithmetic_example(a: Fq, b: Fq) -> Fq {
-    let c = (a + b) * a + b * (a + b) * (a + b);
-    const CONSTANT: Fq = MontFp!("0x12345678901234567890");
-    c * c * c / (b - a) + pow_2(a) + CONSTANT
+pub fn field_arithmetic_example(n: Fq) -> Fq {
+    const ZERO: Fq = MontFp!("0x0");
+    const ONE: Fq = MontFp!("0x1");
+    const TWO: Fq = MontFp!("0x2");
+
+    if n == ZERO {
+        return ZERO;
+    } else if n == ONE {
+        return ONE;
+    }
+
+    field_arithmetic_example(n - ONE) + field_arithmetic_example(n - TWO)
 }
