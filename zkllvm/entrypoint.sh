d3dc4bfc864f8ff29451c61e7a7909da00e11cbe
Mateusz Zając <matzayonc@gmail.com>
2024-08-07 09:53:46 +0200

    generating zkllvm verifier

--- zkllvm/entrypoint.sh
+++ zkllvm/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Compile and generate vrifier
 cargo +zkllvm build --release --target assigner-unknown-unknown --features=zkllvm
+transpiler -m gen-evm-verifier -c circuit.crct -t assignment.tbl -o verifier -e pallas
 
+# Assign inputs
 assigner -b target/assigner-unknown-unknown/release/zkllvm-rust-template.ll -i inputs/example.inp -t assignment.tbl -c circuit.crct -e pallas
 
+# Generate proof
 proof-generator-multi-threaded --circuit circuit.crct --assignment assignment.tbl --proof proof.bin
 
 cat proof.bin
