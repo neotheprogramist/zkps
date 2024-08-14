197ecee4b2c7b8e4790cfc844b8c84083d28adf4
Mateusz Zając <matzayonc@gmail.com>
2024-08-14 15:15:48 +0200

    halo 2 example chip

---
+++ halo2/src/main.rs
@@ -0,0 +1,129 @@
+use chip::{FieldChip, FieldConfig, NumericInstructions};
+use halo2_proofs::{
+    arithmetic::Field,
+    circuit::{Layouter, SimpleFloorPlanner, Value},
+    dev::MockProver,
+    pasta::Fp,
+    plonk::{Circuit, ConstraintSystem, Error},
+};
+
+mod chip;
+
+/// The full circuit implementation.
+///
+/// In this struct we store the private input variables. We use `Option<F>` because
+/// they won't have any value during key generation. During proving, if any of these
+/// were `None` we would get an error.
+#[derive(Default)]
+struct MyCircuit<F: Field> {
+    constant: F,
+    a: Value<F>,
+    b: Value<F>,
+}
+
+impl<F: Field> Circuit<F> for MyCircuit<F> {
+    // Since we are using a single chip for everything, we can just reuse its config.
+    type Config = FieldConfig;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        // We create the two advice columns that FieldChip uses for I/O.
+        let advice = [meta.advice_column(), meta.advice_column()];
+
+        // We also need an instance column to store public inputs.
+        let instance = meta.instance_column();
+
+        // Create a fixed column to load constants.
+        let constant = meta.fixed_column();
+
+        FieldChip::configure(meta, advice, instance, constant)
+    }
+
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), Error> {
+        let field_chip = FieldChip::<F>::construct(config);
+
+        // Load our private values into the circuit.
+        let a = field_chip.load_private(layouter.namespace(|| "load a"), self.a)?;
+        let b = field_chip.load_private(layouter.namespace(|| "load b"), self.b)?;
+
+        // Load the constant factor into the circuit.
+        let constant =
+            field_chip.load_constant(layouter.namespace(|| "load constant"), self.constant)?;
+
+        // We only have access to plain multiplication.
+        // We could implement our circuit as:
+        //     asq  = a*a
+        //     bsq  = b*b
+        //     absq = asq*bsq
+        //     c    = constant*asq*bsq
+        //
+        // but it's more efficient to implement it as:
+        //     ab   = a*b
+        //     absq = ab^2
+        //     c    = constant*absq
+        let ab = field_chip.mul(layouter.namespace(|| "a * b"), a, b)?;
+        let absq = field_chip.mul(layouter.namespace(|| "ab * ab"), ab.clone(), ab)?;
+        let c = field_chip.mul(layouter.namespace(|| "constant * absq"), constant, absq)?;
+
+        // Expose the result as a public input to the circuit.
+        field_chip.expose_public(layouter.namespace(|| "expose c"), c, 0)
+    }
+}
+
+fn main() {
+    // The number of rows in our circuit cannot exceed 2^k. Since our example
+    // circuit is very small, we can pick a very small value here.
+    let k = 4;
+
+    // Prepare the private and public inputs to the circuit!
+    let constant = Fp::from(7);
+    let a = Fp::from(2);
+    let b = Fp::from(3);
+    let c = constant * a.square() * b.square();
+
+    // Instantiate the circuit with the private inputs.
+    let circuit = MyCircuit {
+        constant,
+        a: Value::known(a),
+        b: Value::known(b),
+    };
+
+    use plotters::prelude::*;
+    let root = BitMapBackend::new("layout.png", (1024, 768)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let root = root
+        .titled("Example Circuit Layout", ("sans-serif", 60))
+        .unwrap();
+
+    halo2_proofs::dev::CircuitLayout::default()
+        // You can optionally render only a section of the circuit.
+        .view_width(0..4)
+        .view_height(0..32)
+        // You can hide labels, which can be useful with smaller areas.
+        .show_labels(false)
+        // Render the circuit onto your area!
+        // The first argument is the size parameter for the circuit.
+        .render(5, &circuit, &root)
+        .unwrap();
+
+    // Arrange the public input. We expose the multiplication result in row 0
+    // of the instance column, so we position it there in our public inputs.
+    let mut public_inputs = vec![c];
+
+    // Given the correct public input, our circuit will verify.
+    let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
+    assert_eq!(prover.verify(), Ok(()));
+
+    // If we try some other public input, the proof will fail!
+    public_inputs[0] += Fp::one();
+    let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
+    assert!(prover.verify().is_err());
+}
