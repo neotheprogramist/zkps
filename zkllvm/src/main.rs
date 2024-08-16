ba8ecfe694adba856e9bc17d66733393ffb96576
Mateusz Zając <matzayonc@gmail.com>
2024-08-16 13:23:50 +0200

    made zkllvm compile

--- zkllvm/src/main.rs
+++ zkllvm/src/main.rs
@@ -5,17 +5,21 @@
 use ark_ff::MontFp;
 use ark_pallas::Fq;
 
+const ZERO: Fq = MontFp!("0x0");
+const ONE: Fq = MontFp!("0x1");
+const TWO: Fq = MontFp!("0x2");
+
 #[circuit]
 pub fn field_arithmetic_example(n: Fq) -> Fq {
-    const ZERO: Fq = MontFp!("0x0");
-    const ONE: Fq = MontFp!("0x1");
-    const TWO: Fq = MontFp!("0x2");
+    fib(n)
+}
 
+fn fib(n: Fq) -> Fq {
     if n == ZERO {
         return ZERO;
     } else if n == ONE {
         return ONE;
     }
 
-    field_arithmetic_example(n - ONE) + field_arithmetic_example(n - TWO)
+    fib(n - ONE) + fib(n - TWO) + ZERO
 }
